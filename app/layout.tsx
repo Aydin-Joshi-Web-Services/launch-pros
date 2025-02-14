8b59074a004ea9bbfa6e9bdf0ad5c35866a0fc23
Aydin Joshi <ajoshi1336@gmail.com>
2025-02-14 17:49:10 -0500

    Initial Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter, DM_Sans } from "next/font/google"
+import localFont from "next/font/local"
+import "./globals.css";
+
+const clashgrotesk = localFont({
+  src: "../public/fonts/clash-grotesk.woff2",
+  style: "normal",
+  display: "swap",
+  variable: "--font-clashgrotesk"
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter"
+})
+
+const dmsans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dmsans"
+}) 
+
+export const metadata: Metadata = {
+  title: "SaaS Management for Solopreneuers | LaunchPros",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${dmsans.variable} ${clashgrotesk.variable} dark`}>
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
